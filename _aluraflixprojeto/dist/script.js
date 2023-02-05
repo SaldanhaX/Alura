b690498c9e3024a2ef74a18695b3632fb3b2f370
Rafael Saldanha <119461894+SaldanhaX@users.noreply.github.com>
2023-02-05 17:34:14 -0300

    Add files via upload

---
+++ _aluraflixprojeto/dist/script.js
@@ -0,0 +1,5 @@
+var listaFilmes = ['https://play-lh.googleusercontent.com/em2griqrHoxmxEss_WM5Fi2iqSEKrEfLNAltjX54lREOR0nz0du__KuSi2bA_YNjS4w', 'https://br.web.img2.acsta.net/medias/nmedia/18/90/07/53/20391069.jpg', 'https://assetsgn.nowonline.com.br/assets/p22005_v_v12_bb.jpg', 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/c9a0fb07f49e95406f97ec7a5a8ce10689cc465b5721a40b400adaa559f62bbf._RI_V_TTW_.jpg', 'https://veja.abril.com.br/wp-content/uploads/2020/02/poster-filme-parasite.jpg?quality=70&strip=info', 'https://upload.wikimedia.org/wikipedia/pt/8/82/Pulp_Fiction_cover.jpg']
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write('<img src=' + listaFilmes[i] + '>')
+}
